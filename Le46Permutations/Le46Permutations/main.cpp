edfcf72a36cc93bc9038d6c48a5c2fa94b6b4cab
zhouyu0615 <871211719@qq.com>
2015-12-15 08:55:52 +0800

    Le46

---
+++ Le46Permutations/Le46Permutations/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+class Solution {
+private:
+	vector<vector<int>> result;
+	void swap(int i, int j, vector<int>& nums)
+	{
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+		return;
+	}
+
+	void Dfs(int startPos,vector<int>&nums)
+	{
+		int len = nums.size();
+		if (startPos==(len-1))
+		{
+			result.push_back(nums);
+			return;
+		}
+		else
+		{
+			for (int i = startPos; i < len;i++)
+			{
+				swap(startPos, i, nums);
+				Dfs(startPos+1, nums);
+				swap(startPos, i,nums);
+			}
+		}	
+	}
+
+public:
+	vector<vector<int>> permute(vector<int>& nums) {
+		
+		if (nums.size()==0)
+		{
+			return result;
+		}
+		Dfs(0, nums);
+
+		return result;
+	}
+
+
+};
+
+
+
+int main()
+{
+	Solution test;
+	vector<int> nums = { 1, 2, 3 };
+
+
+
+	vector<vector<int>>::iterator pIter;
+	vector<int>::iterator pI;
+	vector<vector<int>> result;
+	result = test.permute(nums);
+
+
+	for (pIter = result.begin(); pIter != result.end(); ++pIter)
+	{
+		for (pI = pIter->begin(); pI != pIter->end(); ++pI)
+		{
+			cout << *pI << " ";
+		}
+		cout << endl;
+	}
+
+	getchar();
+	return 0;
+}
+
