d48bbfd7efb353ea8c16fa562de4613471c02743
zhouyu0615 <871211719@qq.com>
2015-12-16 20:09:29 +0800

    le282

---
+++ Le282ExpressionAddOperators/Le282ExpressionAddOperators/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<string> addOperators(string num, int target) {
+		vector<int> v_nums;
+
+		for (int i = 0; i < num.length();i++)
+		{
+			v_nums.push_back((num[i] - '0'));
+		}
+
+
+
+	}
+};
+
+
+
+
+
+int main()
+{
+
+
+
+
+	getchar();
+	return 0;
+}
