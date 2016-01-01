e517e9acc4439d652b6be466ff0d0fc257e7679e
zhouyu0615 <871211719@qq.com>
2016-01-01 15:49:24 +0800

    Le62

--- Le62UniquePaths/Le62UniquePaths/main.cpp
+++ Le62UniquePaths/Le62UniquePaths/main.cpp
@@ -6,10 +6,6 @@ class Solution {
 
 public:
 	int uniquePaths(int m, int n) {
-		//int **pathDp;
-		//pathDp = (int **)malloc(sizeof(int *) * m);
-		//for (int i = 0; i < m; i++)
-		//	pathDp[i] = (int *)malloc(sizeof(int) * n);
 
 		m_row = m;
 		m_col = n;
@@ -47,10 +43,58 @@ private:
 	}
 };
 
+
+class Solution2 {
+public:
+	int uniquePaths(int m, int n) {
+		int ** dpPath = new int *[m];
+		for (int i = 0; i < m; i++)
+			dpPath[i] = new int[n];
+
+		dpPath[0][0] = 1;
+
+
+		for (int i = 0; i < m;i++)
+		{
+			for (int j = 0; j < n;j++)
+			{
+				if (i==0&&j==0)
+				{
+					continue;
+				}
+				if ((i==0)&&(j!=0))
+				{
+					dpPath[i][j] = dpPath[i][j-1];
+				}else if ((j==0)&&(i!=0))
+				{
+					dpPath[i][j] = dpPath[i-1][j];
+				}else 
+				{
+					dpPath[i][j] = dpPath[i-1][j] + dpPath[i][j-1];
+				}
+			}
+		}
+
+		int methods = dpPath[m - 1][n - 1];
+
+		for (int i = 0; i < m; i++)
+		{
+			delete dpPath[i];
+			dpPath[i] = NULL;
+		}
+		delete[]  dpPath;
+		dpPath = NULL;
+
+		return methods;
+	}
+};
+
+
+
 int main()
 {
-	Solution testCase;
-	cout << testCase.uniquePaths(3, 7)<<endl;
+	Solution2 testCase;
+	cout << testCase.uniquePaths(12, 12)<<endl;
 
 	getchar();
 	return 0;
