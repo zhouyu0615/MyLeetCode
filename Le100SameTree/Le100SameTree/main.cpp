9529ed40cfe148738081139e7dfa8e261f3a01fb
zhouyu0615 <871211719@qq.com>
2016-01-17 19:17:45 +0800

    Le100

---
+++ Le100SameTree/Le100SameTree/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+/** Definition for a binary tree node.*/
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+	 TreeNode(int x) : val(x), left(NULL), right(NULL) {};
+ };
+
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+	
+		if (p==NULL&&q==NULL)
+		{
+			return true;
+		}else if (p!=NULL&&q!=NULL)
+		{
+			bool left = isSameTree(p->left, q->left);
+			bool right = isSameTree(p->right, q->right);
+			return  (p->val == q->val) && left&&right;
+		}
+		return false;
+	}
+};
