b17a078d27b8c1013ed27191997b62e7ce95aac0
zhouyu0615 <871211719@qq.com>
2016-03-18 11:08:49 +0800

    Le231

---
+++ Le231 Power of Two/Le231 Power of Two/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+	bool isPowerOfTwo(int n) {
+
+
+		return n>0 &&(!(n&n-1));
+	}
+};
+
+
+int main()
+{
+
+	getchar();
+	return 0;
+}
